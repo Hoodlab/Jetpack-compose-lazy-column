fde32f5625858bdce8688f10953611682a9f7f21
Saed saleh <hoods.tech@gmail.com>
2021-08-26 13:17:55 +0300

    Jetpack Compose Lazy Column Starter project

--- app/src/main/java/hoods/com/lazycolumnlayout/MainActivity.kt
+++ app/src/main/java/hoods/com/lazycolumnlayout/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import hoods.com.lazycolumnlayout.ui.theme.LazyColumnLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,22 +17,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LazyColumnLayoutTheme {
-        Greeting("Android")
     }
 }
